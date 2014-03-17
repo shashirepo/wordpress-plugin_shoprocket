5228c61f7e88a4c58ca2ceadf20f4fb76d00f860
shashirepo <shashikantvaishnaw@gmail.com>
2014-03-17 13:39:56 +0530

    remove dead code

--- models/ShoprocketShippingRate.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ShoprocketShippingRate extends ShoprocketModelAbstract {
-  
-  public function __construct($id=null) {
-    $this->_tableName = ShoprocketCommon::getTableName('shipping_rates');
-    parent::__construct($id);
-  }
-  
-}
--- models/ShoprocketShippingRule.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ShoprocketShippingRule extends ShoprocketModelAbstract {
-  
-  public function __construct($id=null) {
-    $this->_tableName = ShoprocketCommon::getTableName('shipping_rules');
-    parent::__construct($id);
-  }
-  
-}
